044899b0f9acc0262a9122e28992ef3277fc729f
Ulises <ulises.mateo.ortega@alumnos.upm.es>
2021-04-10 17:26:59 +0200

    Add RegularMovie

--- src/main/java/usantatecla/movies/v21/Movie.java
+++ src/main/java/usantatecla/movies/v21/Movie.java
@@ -4,14 +4,17 @@ public abstract class Movie {
 
 	private String title;
 
-	
+	private static final int FREQUENT_RENTER_POINTS = 1;
+
 	public Movie(String title) {
 		this.title = title;
 	}
 	
 	public abstract double getCharge(int daysRented);
 	
-	public abstract int getFrequentRenterPoints(int daysRented);
+	public int getFrequentRenterPoints(){
+		return Movie.FREQUENT_RENTER_POINTS;
+	};
 	
 	public String getTitle() {
 		return title;
