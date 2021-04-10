3c25ca9e12ebc1cc77bacba1cc29dcf88d63a4c9
Ulises <ulises.mateo.ortega@alumnos.upm.es>
2021-04-10 17:21:07 +0200

    Change Movie to abstract class

--- src/main/java/usantatecla/movies/v21/Movie.java
+++ src/main/java/usantatecla/movies/v21/Movie.java
@@ -1,27 +1,17 @@
 package usantatecla.movies.v21;
 
-public class Movie {
+public abstract class Movie {
 
 	private String title;
+
 	
-	private Price price;
-	
-	public Movie(String title, Price price) {
+	public Movie(String title) {
 		this.title = title;
-		this.price = price;
 	}
 	
-	public double getCharge(int daysRented) {
-		return price.getCharge(daysRented);
-	}
+	public abstract double getCharge(int daysRented);
 	
-	public int getFrequentRenterPoints(int daysRented) {
-		return price.getFrequentRenterPoints(daysRented);
-	}
-	
-	public void setPrice(Price price) {
-		this.price = price;
-	}
+	public abstract int getFrequentRenterPoints(int daysRented);
 	
 	public String getTitle() {
 		return title;
