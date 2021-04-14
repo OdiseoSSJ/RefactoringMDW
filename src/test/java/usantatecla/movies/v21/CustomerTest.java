fb6e92230a2901f27345751565d9890a51744b72
Ulises <ulises.mateo.ortega@alumnos.upm.es>
2021-04-14 23:17:25 +0200

    Refactor childrensRental1DayTest

--- src/test/java/usantatecla/movies/v21/CustomerTest.java
+++ src/test/java/usantatecla/movies/v21/CustomerTest.java
@@ -153,7 +153,12 @@ public class CustomerTest {
 
 		String result = new StatementBuilder().customerName(customerName).movie(movieName, 1.5)
 				.totalAmount(1.5).frequentRenterPoints(1).build();
-		assertEquals(result, statement);
+
+		assertThat(statement.contains(customerName), is(equalTo(result.contains(customerName))));
+		assertThat(statement.contains(movieName), is(equalTo(result.contains(movieName))));
+		assertThat(statement.contains(amount(1.5)), is(equalTo(result.contains(amount(1.5)))));
+		assertThat(statement.contains(frequentRenterPoints(1)),
+				is(equalTo(result.contains(frequentRenterPoints(1)))));
 	}
 	
 	@Test
